733f348bf689eb4b9353449953481cf1aef062e5
jerivas <jerivas@uaq.edu.mx>
2024-11-21 18:03:55 -0600

    Removed comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from matrix import Matrix
 
+
 def main():
     print("Welcome, this program computes the cross product of two given matrices")
 
@@ -13,25 +14,8 @@ def main():
     B.read()
     B.print()
 
-    C=A.mul(B)
+    C = A.mul(B)
     C.print()
-    
-
-
-    #     for i in range(r1):
-    #         for j in range(c2):
-    #             C[i][j] += A[i][j]*B[j][i]
-
-    #     print("-------------------------------------------------")
-    #     print(f"The resultant matrix is:")
-    #     for i in range(A.r):
-    #         for j in range(B.c):
-    #             print(f"{C.matrix[i][j]}\t", end="")
-    #         print()
-
-    # else:
-    #     print("The given matrices cannot be multiplied!")
-    #     print(f"Matrix A has {A.c} columns and Matrix B has {B.r} rows")
 
 
 if __name__ == "__main__":
