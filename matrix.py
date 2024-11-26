e625d862408d1db601cfa91cda25c23cefb73c23
jerivas <jerivas@uaq.edu.mx>
2024-11-25 18:32:37 -0600

    Add methods to class Matrix

--- matrix.py
+++ matrix.py
@@ -1,5 +1,5 @@
 class Matrix:
-    #r = 0
+    # r = 0
     def __init__(self):
         self.matrix = []
         self.r = 0
@@ -19,7 +19,7 @@ class Matrix:
                 self.matrix[i][j] = int(input())
 
     def print(self):
-        #print(f"The given matrix is:")
+        # print(f"The given matrix is:")
         for i in range(self.r):
             for j in range(self.c):
                 print(f"{self.matrix[i][j]}\t", end="")
@@ -41,17 +41,58 @@ class Matrix:
         else:
             raise Exception("Illegal operation: incompatible dimensions")
 
-    def scalar(self,k):
+    def scalar(self, k):
         R = Matrix()
         R.r = self.r
         R.c = self.c
 
         for i in range(self.r):
             R.matrix.append(list([0] * self.c))
-            
 
         for i in range(self.r):
             for j in range(self.c):
-                R.matrix[i][j] = self.matrix[i][j]*k
+                R.matrix[i][j] = self.matrix[i][j] * k
 
         return R
+
+    def det(self):
+        matrix = self.matrix
+        return (
+            matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
+            - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+            + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0])
+        )
+
+    def adj(self):
+        matrix = self.matrix
+        adj = [[0, 0, 0] for _ in range(3)]
+
+        adj[0][0] = matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]
+        adj[0][1] = -(matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+        adj[0][2] = matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]
+
+        adj[1][0] = -(matrix[0][1] * matrix[2][2] - matrix[0][2] * matrix[2][1])
+        adj[1][1] = matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0]
+        adj[1][2] = -(matrix[0][0] * matrix[2][1] - matrix[0][1] * matrix[2][0])
+
+        adj[2][0] = matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]
+        adj[2][1] = -(matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0])
+        adj[2][2] = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+        return adj
+
+    def inv(self):
+        matrix = self.matrix
+        det = self.det()
+
+        # Si el determinante es 0, la matrix no tiene inversa
+        if det == 0:
+            print("La matrix no tiene inversa (determinante = 0).")
+            return None
+
+        # Calcular la adjunta
+        adj = self.adj()
+
+        # Calcular la inversa (adjunta / determinante)
+        inversa_matrix = [[adj[i][j] / det for j in range(3)] for i in range(3)]
+        return inversa_matrix
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,50 +1,120 @@
 import pytest
 from matrix import Matrix
 
+
 def test_simple():
     A = Matrix()
     A.r = 2
     A.c = 2
-    A.matrix = [[1,2],[3,4]]
+    A.matrix = [[1, 2], [3, 4]]
     B = Matrix()
     B.r = 2
     B.c = 2
-    B.matrix = [[1,0],[0,1]]
+    B.matrix = [[1, 0], [0, 1]]
 
     C = A.mul(B)
 
     assert C.r == 2
     assert C.c == 2
-    assert C.matrix == [[1,2],[3,4]]
+    assert C.matrix == [[1, 2], [3, 4]]
+
 
 def test_3x3():
     A = Matrix()
     A.r = 3
     A.c = 3
-    A.matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    A.matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     B = Matrix()
     B.r = 3
     B.c = 3
-    B.matrix = [[1,0,0],[0,1,0],[0,0,1]]
+    B.matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
     C = A.mul(B)
 
     assert C.r == 3
     assert C.c == 3
-    assert C.matrix == [[1,2,3],[4,5,6],[7,8,9]]
+    assert C.matrix == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 
 def test_incompatibleDimensions():
     with pytest.raises(Exception) as exc_info:
         A = Matrix()
         A.r = 3
         A.c = 3
-        A.matrix = [[1,2,3],[4,5,6],[7,8,9]]
+        A.matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         B = Matrix()
         B.r = 2
         B.c = 2
-        B.matrix = [[1,2],[3,4]]
+        B.matrix = [[1, 2], [3, 4]]
 
         C = A.mul(B)
 
     assert str(exc_info.value) == "Illegal operation: incompatible dimensions"
-    
+
+
+def test_matrixIdentityDet():
+    B = Matrix()
+    B.r = 3
+    B.c = 3
+    B.matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+    d = B.det()
+
+    assert d == 1
+
+
+def test_matrixIdentityDet4x4():
+    B = Matrix()
+    B.r = 4
+    B.c = 4
+    B.matrix = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+
+    d = B.det()
+
+    assert d == 1
+
+
+def test_matrixGives0():
+    B = Matrix()
+    B.r = 3
+    B.c = 3
+    B.matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    d = B.det()
+
+    assert d == 0
+
+
+def test_matrixAdj():
+    B = Matrix()
+    B.r = 3
+    B.c = 3
+    B.matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+    adj = B.adj()
+
+    assert adj == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
+def test_matrixInv():
+    B = Matrix()
+    B.r = 3
+    B.c = 3
+    B.matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+    inv = B.inv()
+
+    assert inv == [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
+def test_matrixInvSimple():
+    B = Matrix()
+    B.r = 3
+    B.c = 3
+    B.matrix = [[5, -2, 2], [1, 0, 1], [3, 1, 4]]
+
+    inv = B.inv()
+
+    expected = [[1, -10, 2], [1, -14, 3], [-1, 11, -2]]
+
+    assert inv == expected
